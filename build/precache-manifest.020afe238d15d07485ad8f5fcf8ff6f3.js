7ca8c6e1608155233941e52f40cc585527232569
Larry Oboh <larry.oboh@outlook.com>
2022-09-19 11:06:03 +0200

    first commit

---
+++ build/precache-manifest.020afe238d15d07485ad8f5fcf8ff6f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e29df0f7bf22a5261ef6c5522a81cf",
+    "url": "/https//Obohlarry.github.io/deltashop.git/index.html"
+  },
+  {
+    "revision": "f012fa634f87bfe1454a",
+    "url": "/https//Obohlarry.github.io/deltashop.git/static/js/2.cd311a34.chunk.js"
+  },
+  {
+    "revision": "bf72086b44f10c47f01e16d9fe377d01",
+    "url": "/https//Obohlarry.github.io/deltashop.git/static/js/2.cd311a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "664a59b2a5adbcf8a67f",
+    "url": "/https//Obohlarry.github.io/deltashop.git/static/js/main.0577e4c1.chunk.js"
+  },
+  {
+    "revision": "db9279f07314b88971e8",
+    "url": "/https//Obohlarry.github.io/deltashop.git/static/js/runtime-main.4acd70ab.js"
+  },
+  {
+    "revision": "d74b1274908acc88a606d4b0341df18d",
+    "url": "/https//Obohlarry.github.io/deltashop.git/static/media/commerce.d74b1274.png"
+  }
+]);
